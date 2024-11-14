dad1f68fcdf107369e50c2b01c47ed98eaecf8c3
Hiba-Mohamed <hibamoha987@gmail.com>
2024-11-14 19:21:18 -0330

    point class added

---
+++ problem2/Point.java
@@ -0,0 +1,42 @@
+public class Point {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(){
+    }
+
+    public float  getX(){
+        return this.x;
+    }
+    public float  getY(){
+        return this.y;
+    }
+      public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public void setXY(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+   public float[] getXY() {
+        return new float[]{this.x, this.y};
+    }
+
+    @Override
+    public String toString(){
+        return "("+this.x+","+this.y+")";
+    }
+
+    
+}
