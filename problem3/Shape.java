c42086bae050a8cc07dfde1c643e75a9b7ff11da
Hiba-Mohamed <hibamoha987@gmail.com>
2024-11-15 13:15:53 -0330

    problem 3 setup

---
+++ problem3/Shape.java
@@ -0,0 +1,21 @@
+package problem3;
+
+public abstract class Shape {
+    //Each shape should have a name, 
+    private String name;
+
+    public Shape(String name){
+        this.name = name;
+    };
+    //a method to compute its perimeter, 
+    public abstract double computePerimeter();
+
+    //and another method to compute its area.
+    public abstract double computeArea();
+
+        @Override
+    public String toString() {
+        return "Shape: " + name + ", Area: " + computeArea() + ", Perimeter: " + computePerimeter();
+    }
+
+}
