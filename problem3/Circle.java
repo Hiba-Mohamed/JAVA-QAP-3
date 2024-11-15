509dc7774ed94909ced2db8b7648fafc6ace90db
Hiba-Mohamed <hibamoha987@gmail.com>
2024-11-15 15:36:28 -0330

    circle class added

--- problem3/Circle.java
+++ problem3/Circle.java
@@ -1,6 +1,15 @@
 package problem3;
 
-public class Circle {
-    
+public class Circle extends Ellipse {
+    private double radius;
+    public Circle (double radius){
+        super(radius, radius);
+        this.name = "Circle";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", radius: " + this.radius;  // Since majorAxis == minorAxis for a circle
+    }
     
 }
